862e1e1926e574e31fcf6472fb801ff704adc62b
helgirg <helgirg@gmail.com>
2014-03-14 13:39:05 +0000

    readme and spilastokkur update

--- spilastokkur.py
+++ spilastokkur.py
@@ -10,58 +10,57 @@ class Card():
 		nameFace = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
 		return "%s%s" % (nameSuit[self.suitNum], nameFace[self.faceNum])
 
-# buum til stokkinn
+# make the deck
 def Make_Deck():
 	deck = []
-	stokk = []
+	shuffle = []
 	for suitNum in range(4):
 		for faceNum in range(13):
 			newCard = Card(faceNum, suitNum)
 			deck.append(newCard)
-	# stokkum stokkinn
+	# shuffle the cards
 	for card in deck:
-		stokk.append(card.getCardName())
-	random.shuffle(stokk)
-	# skilum stokkudum spilastokk
-	return stokk
+		shuffle.append(card.getCardName())
+	random.shuffle(shuffle)
+	# return a shuffled deck
+	return shuffle
 
-# drogum spil og setjum i hand
+# draw a card and put it in our hand
 def Draw_Card(deck, hand):
 	draw = deck[0]
 	hand.append(draw)
 	deck.remove(draw)
 
-# ef seinasta spilid og fjorda seinasta spilid eru somu gerdar fjarlaegjum vid thau sem eru a milli
+# if the last card and the 4th last card have the same suit we take out the cards between them
 def Remove_All(hand):
 		for i in range(-4, -0):
 			hand.remove(hand[i])
 
-# ef seinasta spilid og fjorda seinasta spilid eru sama numer fjarlaegjum vid thau oll
+# if the last card and the 4th last card have the face we remove all 4 cards
 def Remove_Two(hand):
 		for i in range(-3, -1):
 			hand.remove(hand[i])
 
-# faerum seinasta spilid fremst
+# take the last card we have in our hand and put it first
 def Last_First(hand):
 	return hand[1:] + hand[:1]
 
-# fall sem skodar thad sem vid erum med a hendi i hvert skipti
-# eftir ad vid drogum nytt spil ur stokkinum
+# function that checks what to do with the cards we have
+# in our hand each time after we draw a new card
+# and calles relevent functions
 def Check_Loop(hand):
 	if len(hand) >= 4:
-		# ef seinasta spilid og fjorda seinasta spilid eru sama numer fjarlaegjum vid thau oll
 		if hand[-4][1:3]==hand[-1][1:3]:
 			Remove_All(hand)
-		# ef seinasta spilid og fjorda seinasta spilid eru somu gerdar fjarlaegjum vid thau sem eru a milli
 		elif hand[-4][:1]==hand[-1][:1]:
 			Remove_Two(hand)
 			if len(hand) >= 4:
 				if hand[-4][1:3]==hand[-1][1:3]:
 					Remove_All(hand)
 
-# fall sem skodar thad sem vid erum med a hendi
-# thegar stokkurinn okkar er tomur og
-# segir okkur hvort kapallinn gangi upp eda ekki
+# function that check what we can do when we have drawn all cards
+# from the deck and calls relevent functions
+# tells us if we lose ore win
 def Check_Final(hand):
 	if len(hand)==2 or len(hand)==0:
 		#print 'jei'
@@ -79,8 +78,6 @@ def Check_Final(hand):
 			Check_Final(hand)
 		else:
 			print 'kapallinn gekk ekki upp, thu att ' + str(len(hand)) + ' spil eftir'
-	
-
 
 
 
